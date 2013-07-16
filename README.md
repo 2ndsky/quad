5de7b6b27dc83a0ef7b0f47ed7caad8976a918a9
2ndsky <info@nw-software.com>
2013-07-16 11:44:43 +0200

    initial commit of the files

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ quad
 ====
 
 Additional design for the smartVISU. This is a working copy as long as the design is not stable. When it reaches the stable state, it will maybe be integrated into the smartVISU itself.
+
+If you want to use the icon definition for the shutter shifter you have to add two new files in the icon 'ws' folder. In order to do that just execute the two following commands in the smartvisu folder:
+
+cp icons/ws/fts_window_2w.png icons/ws/fts_shutter_0.png
+cp icons/ws/fts_window_2w.png icons/ws/fts_shutter_00.png
+
+This is because of a known bug in the shutter shifter implementation of this design.
---
+++ visu.js
@@ -0,0 +1,51 @@
+// -----------------------------------------------------------------------------
+// W I D G E T   D E L E G A T E   F U N C T I O N S
+// -----------------------------------------------------------------------------
+
+
+
+// ----- v i s u ---------------------------------------------------------------
+// -----------------------------------------------------------------------------
+
+// ----- visu.percent ----------------------------------------------------------
+$(document).delegate('[data-widget="visu.percent"]', {
+        'update': function(event, response) {
+                $('#' + this.id).html(Math.round(parseInt(response) / 255 * 100) + ' %');
+    }
+});
+
+
+// ----- visu.shutter ----------------------------------------------------------
+$(document).delegate('[data-widget="visu.button"]', {
+        'vmousedown': function(event) { // Short/Long Button
+            event.preventDefault();
+	    var items = widget.explode($(this).attr('data-item'));
+            var obj = this;
+            $(obj).attr('data-timer',
+                setTimeout(function() {
+                    $(obj).attr('data-long', true);
+                    io.write(items[1], $(obj).attr('data-val'));
+                }, 400)
+            );
+        },
+        'vmouseup': function() { // Short/Long Button
+            clearTimeout($(this).attr('data-timer'))
+	    var items = widget.explode($(this).attr('data-item'));
+            if ($(this).attr('data-long') == 'true') {
+                $(this).attr('data-long', false);
+            } else {
+	        io.write(items[0], $(this).attr('data-val'));
+            }
+        },
+        'click': function(event) {
+        
+	}
+});
+
+// ----- visu.shifter ----------------------------------------------------------
+$(document).delegate('span[data-widget="visu.shifter"]', { 
+        'update': function(event, response) {
+        var step = Math.min((response / $(this).attr('data-max') * 10 + 0.49).toFixed(0) * 10, 100);
+        $('#' + this.id + ' img').attr('src', $(this).attr('data-pic').replace('00', step));
+    }
+});
