2316fa5fc8bc82403df3dbd1616544be3d74b776
Niko Will <niko.will@avira.com>
2014-02-21 18:57:08 +0100

    Added UZSU widget

--- visu.js
+++ visu.js
@@ -200,3 +200,396 @@ $(document).delegate('[data-widget="visu.map"]', {
     }
 });
 
+// ----- visu.uzsu_icon --------------------------------------------------------
+$(document).delegate('[data-widget="visu.uzsu_icon"]', {
+    'update': function(event, response) {
+        var active = response.length > 0 ? response[0].active : false;
+        $('#' + this.id + ' img').attr('src', (active ? $(this).attr('data-pic-on') : $(this).attr('data-pic-off')));
+        console.log(response);
+
+        if (response.length == 0)
+            return;
+
+		if (response[0].list instanceof Array) {
+            $(this).data('uzsu', response[0]);
+		} else {
+            $(this).data('uzsu', { active: true, list: [] });
+        }
+    },
+    'click': function(event) {
+        var response = $(this).data('uzsu');
+        var type = $(this).attr('data-type');
+        var min = $(this).attr('data-min');
+        var max = $(this).attr('data-max');
+        var items = widget.explode($(this).attr('data-item'))
+        
+        uzsu.open(type, response.active, response.list, items, min, max);
+	}
+});
+
+
+var UzsuPopup = function() {
+    this.init();
+}
+
+UzsuPopup.prototype = {
+    // true, if the UZSU is active, false otherwise
+    active: true,
+
+    // list of the backend items
+    items: [],
+
+    // the list of all uzsu entries
+    list: [],
+
+    // dialog history
+    history: [],
+
+    img: {
+        active: '',
+        inactive: ''
+    },
+
+    // the value type
+    type: 'bool',
+
+    current: {
+        // the current visible panel 
+        panel: '',
+
+        // the current list entry
+        entry: {},
+
+        // if the current entry is new
+        isNew: false,
+
+        // the list index of the current entry
+        // only valid if isNew is false
+        index: 0
+    },
+
+    // variable names from the HTML structure
+    vars: {
+        popup: '#popupUZSU',
+        main: '#panel_list',
+        list: '#uzsu-items',
+        dateType: '#panel_date_type',
+        timeType: '#panel_time_type',
+        time: '#panel_time',
+        valuePanelPrefix: '#panel_value_',
+    },
+
+    init: function() {
+        // add events
+        $(this.vars.main + ' .close').on('click', $.proxy(this.close, this));
+        $(this.vars.main + ' .ok').on('click', $.proxy(this.save, this));
+        $(this.vars.main + ' .add').on('click', $.proxy(this.add, this));
+        $(this.vars.popup + ' .prev').on('click', $.proxy(this.prev, this));
+
+        $(this.vars.main).on('click', 'li', $.proxy(function(e) {
+            this.change($(this.vars.main + ' li').index($(e.target).closest('li')));
+        }, this));
+
+        $(this.vars.popup).on('popupbeforeposition', $.proxy(this.next, this));
+
+        // init panels
+        $.proxy(this.panels.time.init, this)();
+        $.proxy(this.panels.dateType.init, this)();
+        $.proxy(this.panels.timeType.init, this)();
+        $.proxy(this.panels.values.percent.init, this)();
+        $.proxy(this.panels.values.boolean.init, this)();
+    },
+
+    open: function(type, active, list, items, min, max) {
+        this.min = min.length > 0 ? parseInt(min) : 0;
+        this.max = max.length > 0 ? parseInt(max) : 255;
+        this.list = list;
+        this.type = type;
+        this.active = active;
+        this.history = [];
+        this.current.panel = '';
+
+        if (items instanceof Array) {
+            this.items = items;
+        } else {
+            this.items = [ items ];
+        }
+
+        $(this.vars.popup).popup('open');
+    },
+
+    close: function() {
+        $(this.vars.popup).popup('close');
+    },
+
+    refresh: function() {
+        var ret = '', line = '';
+
+        // clear the list
+        $(this.vars.list).empty();
+
+	    for (var i = 0; i < this.list.length; i++) {
+            var item = this.list[i];
+            ret = '';
+            // ret += '<img class="icon ui-li-thumb" src="' + (item.active ? this.img.active : this.img.inactive) + '" />';
+            ret += '<h3>';
+            if (item.rrule) {
+                if (item.rrule == 'FREQ=DAILY')
+                    ret += 'Täglich';
+                else if (item.rrule == 'FREQ=WEEKLY;WKST=SU;BYDAY=MO,TU,WE,TH,FR')
+                    ret += 'Werktage';
+                else if (item.rrule == 'FREQ=WEEKLY;WKST=SU;BYDAY=SA,SU')
+                    ret += 'Wochenende';
+                else
+                    ret += 'Benutzerdefiniert'
+            }
+            ret += ' um ' + item.time.replace('<','&lt') + ' Uhr</h3>';
+		    //ret += '<a href="#delete">Löschen</a>';
+            line += '<li data-icon="false"><a>' + ret + '</a></li>';
+        }
+
+        // set the actual content
+	    $(this.vars.list).html(line).trigger('prepare').listview('refresh').trigger('redraw');
+    },
+
+    save: function() {
+        for (var i = 0; i < this.items.length; i++) {
+            io.write(this.items[i], { active: this.active, list: this.list });
+        }
+        this.close();
+    },
+
+    next: function(value) {
+        var panel = this.vars.main;
+
+        switch (this.current.panel) {
+            case this.vars.main:
+                panel = this.vars.valuePanelPrefix + this.type;
+                break;
+
+            // result of the date type chooser
+            case this.vars.dateType:
+                switch (value) {
+                    case 0:
+                        this.current.entry.rrule = 'FREQ=DAILY';
+                        panel = this.vars.timeType;
+                        break;
+                    case 1:
+                        this.current.entry.rrule = 'FREQ=WEEKLY;WKST=SU;BYDAY=MO,TU,WE,TH,FR';
+                        panel = this.vars.timeType;
+                        break;
+                    case 2:
+                        this.current.entry.rrule = 'FREQ=WEEKLY;WKST=SU;BYDAY=SA,SU';
+                        panel = this.vars.timeType;
+                        break;
+                }
+                break;
+
+            // result of the time type chooser
+            case this.vars.timeType:
+                switch (value) {
+                    case 0:
+                        panel = this.vars.time;
+                        break;
+                }
+                break;
+
+            // result of the time chooser
+            case this.vars.time:
+                var value = this.current.entry.time;
+                if (value.length != 4)
+                    return;
+                this.current.entry.time = value.substr(0,2) + ':' + value.substr(2,2);
+                if (this.current.isNew) {
+                    this.list.push(this.current.entry);
+                } else {
+                    this.list[this.current.index] = this.current.entry;
+                }                
+                break;
+
+            // result of a value panel
+            case this.panels.values.percent.id:
+            case this.panels.values.boolean.id:
+                panel = this.vars.dateType;
+                break;
+        }
+
+        $(this.vars.popup + ' > div').hide();
+
+        // if the next panel ist the main panel
+        // a new entry was added to the list
+        if (panel == this.vars.main) {
+            this.history = [];
+            this.refresh();
+        } else {
+            this.history.push(this.current.panel);
+        }
+
+        // show the panel
+        $(panel).show().trigger('change');
+        
+        // remember the current panel
+        this.current.panel = panel;
+    },
+
+    prev: function() {
+        panel = this.history.pop();
+        if (panel) {
+            $(this.vars.popup + ' > div').hide();
+            $(panel).show().trigger('change');
+            this.current.panel = panel;
+        }
+    },
+
+    change: function(index) {
+        this.current.isNew = false;
+        this.current.index = index;
+        this.current.entry = $.extend({}, this.list[index]);
+        this.next();
+    },
+
+    add: function() {
+        this.current.isNew = true;
+        this.current.entry = { value: 0, time: '', active: true, rrule: '' };
+        this.next();
+    },
+
+    panels: {
+        dateType: {
+            init: function() {
+                $(this.vars.dateType).on('click', 'li', $.proxy(function(e) {
+                    this.next.call(this, $(this.vars.dateType + ' li').index($(e.target)));
+                }, this));
+            }
+        },
+
+        timeType: {
+            init: function() {
+                $(this.vars.timeType).on('click', 'li', $.proxy(function(e) {
+                    this.next.call(this, $(this.vars.timeType + ' li').index($(e.target)));
+                }, this));
+            }
+        },
+
+        time: {
+            init: function() {
+                $(this.vars.time).on('change', $.proxy(this.panels.time.refresh, this));
+                $(this.vars.time + ' .next').on('click', $.proxy(this.next, this));
+                $(this.vars.time + ' .number_button').on('click', $.proxy(function(e) {
+                    this.panels.time.btnClicked.call(this, $(e.target).text());
+                }, this));
+            },
+
+            btnClicked: function(txt) {
+                var value = this.current.entry.time;
+
+                if (txt == 'AC')
+                    value = '';
+                else if (txt == 'C')
+                    value = value.substring(0, value.length - 1);
+                else {
+                    switch (value.length) {
+                        case 0:
+                            if (parseInt(txt) <= 2)
+                                value += txt;
+                            break;
+                        case 1:
+                            if (parseInt(value) <= 1)
+                                value += txt;
+                            else if (parseInt(txt) <= 3)
+                                value += txt;
+                            break;
+                        case 2:
+                            if (parseInt(txt) <= 5)
+                                value += txt;
+                            break;
+                        case 3:
+                            value += txt;
+                            break;
+                    }
+                }
+
+                this.current.entry.time = value;
+                $(this.vars.time).trigger('change');
+            },
+
+            refresh: function() {
+                var value = this.current.entry.time;
+                var text = '__:__';
+                switch (value.length) {
+                    case 1: text = value + '_:__'; break;
+                    case 2: text = value + ':__'; break;
+                    case 3: text = value.substr(0,2) + ':' + value.substr(2,1) + '_'; break;
+                    case 4: text = value.substr(0,2) + ':' + value.substr(2,2); break;
+                    case 5: text = value; this.current.entry.time = value.substr(0,2) + value.substr(3,2); break;
+                }
+                $(this.vars.time + ' .numpad_value span:first').text(text + ' Uhr');
+            }
+        },
+
+        values: {
+            percent: {
+                id: '#panel_value_percent',
+
+                init: function() {
+                    $(this.panels.values.percent.id).on('change', $.proxy(this.panels.values.percent.refresh, this));
+                    $(this.panels.values.percent.id + ' .next').on('click', $.proxy(this.next, this));
+                    $(this.panels.values.percent.id + ' .number_button').on('click', $.proxy(function(e) {
+                        this.panels.values.percent.btnClicked.call(this, $(e.target).text());
+                    }, this));
+                },
+
+                btnClicked: function(txt) {
+                    var value = this.current.entry.value == 0 ? 0 : Math.round(this.current.entry.value / this.max * 100);
+                    value = value.toString();
+
+                    if (txt == 'AC')
+                        value = '0';
+                    else if (txt == 'C')
+                        value = Math.round(value / 10);
+                    else if (value == '0')
+                        value = txt;
+                    else
+                        value += txt;
+                    
+                    // validation checks
+                    if (parseInt(value) > 100)
+                        value = value.substring(0, value.length - 1);
+                    
+                    this.current.entry.value = Math.round(this.max / 100 * parseInt(value));
+                    $(this.panels.values.percent.id).trigger('change');
+                },
+
+                refresh: function() {
+                    var value = Math.round(this.current.entry.value / this.max * 100)
+                    $(this.panels.values.percent.id + ' .numpad_value span:first').text(value + " %");
+                }
+            },
+
+            boolean: {
+                id: '#panel_value_bool',
+
+                init: function() {
+                    $(this.panels.values.boolean.id).on('change', $.proxy(this.panels.values.boolean.refresh, this));
+                    $(this.panels.values.boolean.id + ' select').on('slidestop', $.proxy(this.panels.values.boolean.slidestop, this));
+                    $(this.panels.values.boolean.id + ' .next').on('click', $.proxy(this.next, this));
+                },
+
+                refresh: function(e) {
+                    if ($(e.target).is($(this.panels.values.boolean.id)))                    
+                        $(this.panels.values.boolean.id + ' > div > select').val(this.current.entry.value).slider("refresh");
+                },
+
+                slidestop: function() {
+                    this.current.entry.value = $(this.panels.values.boolean.id + ' > div > select').val();
+                }
+            }
+        }
+    }
+};
+
+var uzsu;
+
+$(document).ready(function() {
+    uzsu = new UzsuPopup();
+});
